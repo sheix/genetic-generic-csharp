16919630dc3b65eb07ab8d482cf32286dd27cfac
Anton Sheihman <anton.sheihman@gmail.com>
2014-08-17 22:44:24 +0300

    Started work on waveforms

---
+++ DSP/Wave.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSP
+{
+    public abstract class Wave
+    {
+    }
+
+    public class SquareWave : Wave
+    {
+        public int Height;
+    }
+
+}
---
+++ Engine/DSPTest/SquareWaveShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DSP;
+using NUnit.Framework;
+
+namespace DSPTest
+{
+    [TestFixture]
+    public class SquareWaveShould
+    {
+        [Test]
+        public void BeAWave()
+        {
+            SquareWave squareWave = new SquareWave();
+            Assert.IsInstanceOf<Wave>(squareWave);
+        }
+
+        [Test]
+        public void HaveHeightAndWidth()
+        {
+            SquareWave squareWave = new SquareWave();
+            Assert.IsTrue(squareWave.Height>0);
+            //Assert.IsTrue(squareWave.Height>0);
+        }
+    }
+
+    }
